12d0b7f3a97be1cb2fec6b7a7bf5e464cb17354f
Noelia Muñiz Menéndez <nmunizmenend@gmail.com>
2019-01-14 23:13:11 +0100

    Adding blog and categories models to Django admin and linking post with blog and category

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from blogs.models import Blog
+from categories.models import Category
 from posts.models import Post
 
 admin.site.register(Post)
+admin.site.register(Blog)
+admin.site.register(Category)
--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+from blogs.models import Blog
+from categories.models import Category
+
 
 class Post(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     resume = models.TextField()
     body = models.TextField()
@@ -9,6 +13,7 @@ class Post(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     pub_date = models.DateTimeField()
     last_modification = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
